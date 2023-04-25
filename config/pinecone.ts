a06e96f0caeb52c0598fdb0979ed01d7b6a85051
mvsbm <sungbinma@gmail.com>
2023-04-25 14:48:05 +0900

    Minor Fix

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -8,6 +8,6 @@ if (!process.env.PINECONE_INDEX_NAME) {
 
 const PINECONE_INDEX_NAME = 'langchain-pdf';
 
-const PINECONE_NAME_SPACE = 'tesla'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = 'blackrock'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
